750a046c50b942a2e256dba0c21769aca358310f
akanshsingh9175-oss <akanshsingh9175@gmail.com>
2026-09-08 12:06:05 +0530

    Add VPC and subnet resources to main.tf

--- main.tf
+++ main.tf
@@ -17,9 +17,27 @@ provider "aws" {
   region = var.aws_region
 }
 
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "github-actions-demo-vpc"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "github-actions-demo-subnet"
+  }
+}
+
 resource "aws_instance" "web" {
   ami           = var.ami_id
   instance_type = var.instance_type
+  subnet_id     = aws_subnet.main.id
 
   tags = {
     Name = "github-actions-demo-vm"
